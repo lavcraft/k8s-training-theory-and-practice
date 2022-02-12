23725a741d18ff07cada0789fbd98511c663edcc
Tolkachev Kirill <lavcraft@gmail.com>
2022-02-13 00:09:45 +0400

    fix: fix styles loading - fix prefix relation

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const isProduction = process.env.NODE_ENV === 'production'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProduction ? 'k8s-training-theory-and-practice' : '',
+  assetPrefix: isProduction ? '/k8s-training-theory-and-practice/' : '',
 }
 
 module.exports = nextConfig
