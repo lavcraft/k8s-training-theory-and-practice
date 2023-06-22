588a38381614e4b94eaa5caeaea8b80641a2bd0b
Tolkachev Kirill <lavcraft@gmail.com>
2023-06-22 19:05:43 +0600

    chore: fix type problem

--- src/helpers/browser-clipboard.ts
+++ src/helpers/browser-clipboard.ts
@@ -3,7 +3,7 @@ export async function copyToClipboard(data: string) {
         return
     }
 
-    await navigator.permissions.query({name: "clipboard-write"}).then(async (result) => {
+    await navigator.permissions.query(<PermissionDescriptor>{name: "clipboard-write"}).then(async (result) => {
 
         if (result.state === "granted" || result.state === "prompt") {
             /* write to the clipboard now */
